d030a9ac68041a3b2ad69dd74bdebf579997fa87
hamu1011 <akang.mufti@gmail.com>
2020-12-20 09:55:13 +0100

    Change MQTT messaging information for lightshow and singlepixels

--- MQTTHandler.cpp
+++ MQTTHandler.cpp
@@ -1,9 +1,6 @@
-// 
-// 
-// 
-
 #include <ArduinoJson.h>
 #include "8x8LEDHandler.h"
+
 
 void sendESPStatus(uint32_t periode) {
 	static long lastTime = millis();
@@ -16,6 +13,7 @@ void sendESPStatus(uint32_t periode) {
 	}
 }
 
+
 void onTXState() {
 	// { "ADR": MAC_ADRESS, "BAT": value, "LDR" : value}
 	StaticJsonDocument<100> toSend;
@@ -26,6 +24,7 @@ void onTXState() {
 	size_t n = serializeJson(toSend, buffer);
 	client->publish("LED88ESP32/State", buffer, n);
 }
+
 
 void onRxCommand(const String& message) {
 	// JSON Message: { "cmd": LightOff, "adr": "FF22DDAA0011" }
@@ -57,17 +56,17 @@ void onRxCommand(const String& message) {
 		else if (cmd.equals("TapToLight")){
 			CurrentState = TapToLight;
 		}
-		else if (cmd.equals("LightOff")){
+		else if (cmd.equals("LightShow")){
 			CurrentState = LightShow;
+		}
+		else if (cmd.equals("SingleColor")) {
+			CurrentState = SingleColor;
 		}
 		else if (cmd.equals("GetAddress")) {
 			client->publish("LED88ESP32/Address", MAC_ADR);
 		}
 		else if (cmd.equals("GetState")){
 			onTXState();
-		}
-		else if (cmd.equals("Adaptable")) {
-			IS_ADAPTABLE_TO_LIGHT = !IS_ADAPTABLE_TO_LIGHT;
 		}
 		else
 		{
@@ -77,14 +76,21 @@ void onRxCommand(const String& message) {
 
 }
 
+
 void onRxBrightness(const String& message) {
-	// JSON Message: "{ "br": 255 }"
-	static char msg[20];
+	// JSON Message: "{ "br": 255,  "adp": 1 }"
+	static char msg[30];
 	message.toCharArray(msg, 20);
 	StaticJsonDocument<20> receivedMsg;
 	deserializeJson(receivedMsg, msg);
 	BRIGHTNESS = receivedMsg["br"];
+	if (IS_ADAPTABLE_TO_LIGHT != bool(receivedMsg["adp"]))
+	{
+		IS_ADAPTABLE_TO_LIGHT = receivedMsg["adp"];
+	}
+	
 }
+
 
 void onRxTextGenerator(const String& message) {
 	// JSON Message: { "txt": "sometext", "r": 255, "g": 255, "b": 255, "spd" : 255 }
@@ -102,6 +108,7 @@ void onRxTextGenerator(const String& message) {
 	TXT_SPEED = map(receivedMsg["spd"], 50, 255, 150, 50);
 }
 
+
 void onRxPixels(const String& message) {
 	// JSON Message: { "col": 8, "row": 8, "on": 1, "r": 255, "g": 255, "b": 255 }
 	static char msg[100];
@@ -112,8 +119,49 @@ void onRxPixels(const String& message) {
 
 	byte col = receivedMsg["col"];
 	byte row = receivedMsg["row"];
-	PX_SELECT[col][row] = receivedMsg["on"];;
+	PX_SELECT[col][row] = receivedMsg["on"];
 	PX_COLORS[col][row][0] = receivedMsg["r"];
 	PX_COLORS[col][row][1] = receivedMsg["g"];
 	PX_COLORS[col][row][2] = receivedMsg["b"];
+}
+
+
+void onRxLightShow(const String& message) {
+	// Json Message: "{"ptr": 16}"
+	static char msg[20];
+	message.toCharArray(msg, 20);
+
+	StaticJsonDocument<100> receivedMsg;
+	deserializeJson(receivedMsg, msg);
+
+	PATTERN = receivedMsg["ptr"];
+
+}
+
+void onRxSingleColorSetColor(const String& message) {
+	// Json Message: " {"sel": 16, "r": 255, "g": 255, "b": 255} "
+	static char msg[50];
+	message.toCharArray(msg, 50);
+
+	StaticJsonDocument<100> receivedMsg;
+	deserializeJson(receivedMsg, msg);
+
+	byte selection = receivedMsg["sel"];
+	if (ESP_NO == selection)
+	{
+		SINGLECOLOR.red = receivedMsg["r"];
+		SINGLECOLOR.green = receivedMsg["g"];
+		SINGLECOLOR.blue = receivedMsg["b"];
+	}
+}
+
+void onRxSingleColorSetSequence(const String& message) {
+	// Json Message: "{"seq": 3}"
+	static char msg[20];
+	message.toCharArray(msg, 20);
+
+	StaticJsonDocument<100> receivedMsg;
+	deserializeJson(receivedMsg, msg);
+
+	SINGLECOLOR.sequence = receivedMsg["seq"];
 }
