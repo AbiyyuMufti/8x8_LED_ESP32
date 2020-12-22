a47df451ca9a5edad295791d07400ae04cec2978
hamu1011 <akang.mufti@gmail.com>
2020-12-22 07:55:42 +0100

    calculating LDR and SOC Battery in percen with map function

--- EnergySaver.cpp
+++ EnergySaver.cpp
@@ -8,31 +8,43 @@ q : Process Noise
 */
 SimpleKalmanFilter ldrFilter(10, 2, 0.01);
 SimpleKalmanFilter batFilter(10, 2, 0.01);
+unsigned int LDRValUnFil = 0;
+unsigned int BattUnFil = 0;
+unsigned int LDRValFil = 0, BattFil = 0;
+
+
+void calculateESPState()
+{
+	BattUnFil = analogRead(PIN_BATT);
+	LDRValUnFil = analogRead(PIN_LDR);
+
+	BattFil = batFilter.updateEstimate(analogRead(PIN_BATT));
+	LDRValFil = ldrFilter.updateEstimate(analogRead(PIN_LDR));
+
+	ESPINFO.BatteryState = map(BattFil, 0, 4094, 0, 100);
+	ESPINFO.LDRValue = map(LDRValFil, 0, 4094, 0, 100);
+}
 
 void brightnessControl() {
-	//Define Variables we'll be connecting to
 	static double Setpoint, Input, Output;
-	//Specify the links and initial tuning parameters
 	static const double Kp = 0.1, Ki = 0.0001, Kd = 0;
 	static PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, REVERSE);
-	static int oldBrightness = matrix->getBrightness();
-	static unsigned int LDRValUnFil = 0;
-	static unsigned int BattUnFil = 0;
+
+	static int oldBrightness = matrix->gamma8(matrix->getBrightness());
 	static bool called_once = true;
-	BatteryState = batFilter.updateEstimate(analogRead(PIN_BATT));
-	BattUnFil = analogRead(PIN_BATT);
-	LDRValue = ldrFilter.updateEstimate(analogRead(PIN_LDR));
-	LDRValUnFil = analogRead(PIN_LDR);
-	Input = (double)LDRValue;
+	
+	calculateESPState();
+	
+	Input = (double)LDRValFil;
 	if (IS_ADAPTABLE_TO_LIGHT)
 	{
 		if (!called_once)
 		{
 			// Set to automatic
 			Serial.println("Set to automatic");
-			Setpoint = 15;
+			Setpoint = 10;
 			myPID.SetMode(AUTOMATIC);
-			myPID.SetOutputLimits(1, 255);
+			myPID.SetOutputLimits(5, 255);
 			called_once = true;
 		}
 		myPID.Compute();
@@ -49,23 +61,31 @@ void brightnessControl() {
 		}
 		if (oldBrightness != BRIGHTNESS) {
 			matrix->setBrightness(BRIGHTNESS);
+			oldBrightness = BRIGHTNESS;
 		}
 	}
 	// Uncomment to see the value inside !!!
 	/*Serial.print("Akt: ");
-	Serial.print(matrix->getBrightness());
+	Serial.print(matrix->gamma8(matrix->getBrightness()));
 	Serial.print(" Set: ");
 	Serial.print(Setpoint);
 	Serial.print(" In: ");
 	Serial.print(Input);
 	Serial.print(" Out: ");
-	Serial.println(Output);*/
+	Serial.println(matrix->gamma8((uint8_t)Output));*/
 	/*Serial.print("Bat - unfiltered: ");
 	Serial.print(BattUnFil);
 	Serial.print(" filtered: ");
-	Serial.print(BatteryState);
+	Serial.print(BattFil);
+	Serial.print(" - ");
+	Serial.print(ESPINFO.BatteryState);
+	Serial.print(" % ");
 	Serial.print("\tLDR - unfiltered: ");
 	Serial.print(LDRValUnFil);
 	Serial.print(" filterd: ");
-	Serial.println(LDRValue);*/
+	Serial.print(LDRValFil);
+	Serial.print(" - ");
+	Serial.print(ESPINFO.LDRValue);
+	Serial.print(" % ");
+	Serial.println();*/
 }
